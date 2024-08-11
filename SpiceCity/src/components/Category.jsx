bd22e00f07e9ae4697c4b41e73eb83dd0d77d6ec
Aftab073 <tamboliaftab84@gmail.com>
2024-08-12 00:17:51 +0530

    Add Categories

--- SpiceCity/src/components/Category.jsx
+++ SpiceCity/src/components/Category.jsx
@@ -8,7 +8,7 @@ const Category = () => {
             {/* Categories */}
             <div className="grid grid-cols-2  md:grid-cols-4 gap-6 py-6">
                 {categories.map((item, index) => (
-                    <div key={index} className="bg-gray-100 rounded-xl p-4 flex justify-between items-center">
+                    <div key={index} className="bg-gray-100 rounded-xl p-4 flex justify-between items-center hover:shadow-lg hover:shadow-orange-500/50 ">
                         <h2 className="font-bold sm:text-xl"> {item.name}</h2>
                         <img src={item.image} alt={item.name} 
                         className="w-20 "/>
